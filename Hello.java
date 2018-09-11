26a595e0088c6702b0368c0e008f2677a64b9776
dongpham120899 <38093371+dongpham120899@users.noreply.github.com>
2018-09-11 14:38:44 +0700

    Hello

---
+++ Hello.java
@@ -0,0 +1,11 @@
+
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World,Java");
+    }
+    
+}
